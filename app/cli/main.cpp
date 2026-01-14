9c8a6f70cf2171c945062dedc877c363b0dbdfe6
pans0ul <pans0ul@163.com>
2026-01-14 16:19:50 +0800

    feat: support pass email setting by config file and parameters

--- app/cli/main.cpp
+++ app/cli/main.cpp
@@ -4,12 +4,24 @@
 #include <QFile>
 #include <QDateTime>
 #include <QSettings>
+#include <QCommandLineParser>
 
 #include "../../src/SimpleMail"
-#include "../account_info.h"  
+#include "../account_info.h"
 
 using namespace SimpleMail;
 
+struct MailConfig {
+    QString smtpServer;
+    int smtpPort = 465;
+    QString senderName;
+    QString senderEmail;
+    QString senderPassword;
+    QString recipientName;
+    QString recipientEmail;
+};
+
+MailConfig mailConfig;
 QStringList emailContents;
 int currentIndex = 0;
 int maxCount = 0;
@@ -17,6 +29,23 @@ int linesPerEmail = 1;
 int retryCount = 0;
 const int MAX_RETRIES = 3;
 QString progressFile = "mail_progress.ini";
+
+bool loadConfigFromFile(const QString &filename, MailConfig &config) {
+    QSettings settings(filename, QSettings::IniFormat);
+    if (!QFile::exists(filename)) {
+        return false;
+    }
+    
+    config.smtpServer = settings.value("smtp/server").toString();
+    config.smtpPort = settings.value("smtp/port", 465).toInt();
+    config.senderName = settings.value("sender/name").toString();
+    config.senderEmail = settings.value("sender/email").toString();
+    config.senderPassword = settings.value("sender/password").toString();
+    config.recipientName = settings.value("recipient/name").toString();
+    config.recipientEmail = settings.value("recipient/email").toString();
+    
+    return !config.smtpServer.isEmpty() && !config.senderEmail.isEmpty() && !config.recipientEmail.isEmpty();
+}
 
 QStringList readTxtFile(const QString &filename) {
     QStringList lines;
@@ -78,7 +107,7 @@ QStringList getEmailContent(int index) {
 void printSchedule(qint64 intervalMs, int count) {
     qDebug() << "=== 邮件发送计划 ===";
     qDebug() << "总邮件数量:" << count;
-    qDebug() << "每封邮件行数:" << linesPerEmail;
+    qDebug() << "文件内容读取行数:" << linesPerEmail;
     qDebug() << "发送间隔:" << intervalMs << "毫秒";
     qDebug() << "当前进度: 从第" << (currentIndex + 1) << "封开始";
     
@@ -106,16 +135,16 @@ int workerSendMail() {
     qDebug() << QString("发送第%1封邮件 (重试:%2/%3)").arg(currentIndex + 1).arg(retryCount).arg(MAX_RETRIES);
     
     Server server;
-    server.setHost(QLatin1String(SMTP_SERVER));
-    server.setPort(SMTP_SERVER_PORT);
+    server.setHost(mailConfig.smtpServer);
+    server.setPort(mailConfig.smtpPort);
     server.setConnectionType(Server::SslConnection);
-    server.setUsername(QLatin1String(SENDER_EMAIL));
-    server.setPassword(QLatin1String(SENDER_PASSWORD));
+    server.setUsername(mailConfig.senderEmail);
+    server.setPassword(mailConfig.senderPassword);
 
     MimeMessage message;
-    EmailAddress sender(QLatin1String(SENDER_EMAIL), QLatin1String(SENDER_NAME));
+    EmailAddress sender(mailConfig.senderEmail, mailConfig.senderName);
     message.setSender(sender);
-    EmailAddress to(QLatin1String(RECIPIENT_EMAIL), QLatin1String("Recipient's Name"));
+    EmailAddress to(mailConfig.recipientEmail, mailConfig.recipientName);
     message.addTo(to);
     message.setSubject(QString("邮件 #%1").arg(currentIndex + 1));
 
@@ -179,32 +208,120 @@ int workerSendMail() {
 
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
+    app.setApplicationName("SimpleMail CLI");
+    app.setApplicationVersion("1.0");
 
-    if (argc < 5) {
-        qDebug() << "用法: myapp <txt文件路径> <发送间隔> <发送数量> <每封邮件行数> [--reset]";
-        qDebug() << "时间单位: 1d(天) 1h(小时) 1m(分钟) 1s(秒) 1000(毫秒)";
-        qDebug() << "--reset: 重置发送进度，从头开始";
-        return -1;
+    QCommandLineParser parser;
+    parser.setApplicationDescription("SimpleMail 命令行邮件发送工具");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.addPositionalArgument("content", "邮件内容文件路径");
+    parser.addPositionalArgument("interval", "发送间隔 (1d/1h/1m/1s/1000ms)");
+    parser.addPositionalArgument("count", "发送数量");
+    parser.addPositionalArgument("lines", "文件内容读取行数");
+
+    QCommandLineOption configOption(QStringList() << "c" << "config", "配置文件路径", "file");
+    QCommandLineOption smtpServerOption("smtp-server", "SMTP服务器地址", "server");
+    QCommandLineOption smtpPortOption("smtp-port", "SMTP端口", "port", "465");
+    QCommandLineOption senderNameOption("sender-name", "发件人名称", "name");
+    QCommandLineOption senderEmailOption("sender-email", "发件人邮箱", "email");
+    QCommandLineOption senderPasswordOption("sender-password", "发件人密码", "password");
+    QCommandLineOption recipientNameOption("recipient-name", "收件人名称", "name");
+    QCommandLineOption recipientEmailOption("recipient-email", "收件人邮箱", "email");
+    QCommandLineOption resetOption("reset", "重置发送进度");
+
+    parser.addOption(configOption);
+    parser.addOption(smtpServerOption);
+    parser.addOption(smtpPortOption);
+    parser.addOption(senderNameOption);
+    parser.addOption(senderEmailOption);
+    parser.addOption(senderPasswordOption);
+    parser.addOption(recipientNameOption);
+    parser.addOption(recipientEmailOption);
+    parser.addOption(resetOption);
+
+    parser.process(app);
+
+    const QStringList args = parser.positionalArguments();
+    if (args.size() < 4) {
+        parser.showHelp(1);
     }
 
-    // 检查是否需要重置进度
-    bool resetProgress = false;
-    for (int i = 1; i < argc; ++i) {
-        if (QString(argv[i]) == "--reset") {
-            resetProgress = true;
-            break;
+    // 加载配置：优先级 命令行参数 > 配置文件 > 头文件默认值
+    bool hasConfig = false;
+    
+    if (parser.isSet(configOption)) {
+        if (loadConfigFromFile(parser.value(configOption), mailConfig)) {
+            hasConfig = true;
+        } else {
+            qDebug() << "无法加载配置文件或配置不完整";
+            return -1;
         }
     }
 
-    if (resetProgress) {
+    // 命令行参数覆盖配置文件
+    if (parser.isSet(smtpServerOption)) {
+        mailConfig.smtpServer = parser.value(smtpServerOption);
+        hasConfig = true;
+    }
+    if (parser.isSet(smtpPortOption)) {
+        mailConfig.smtpPort = parser.value(smtpPortOption).toInt();
+        hasConfig = true;
+    }
+    if (parser.isSet(senderNameOption)) {
+        mailConfig.senderName = parser.value(senderNameOption);
+        hasConfig = true;
+    }
+    if (parser.isSet(senderEmailOption)) {
+        mailConfig.senderEmail = parser.value(senderEmailOption);
+        hasConfig = true;
+    }
+    if (parser.isSet(senderPasswordOption)) {
+        mailConfig.senderPassword = parser.value(senderPasswordOption);
+        hasConfig = true;
+    }
+    if (parser.isSet(recipientNameOption)) {
+        mailConfig.recipientName = parser.value(recipientNameOption);
+        hasConfig = true;
+    }
+    if (parser.isSet(recipientEmailOption)) {
+        mailConfig.recipientEmail = parser.value(recipientEmailOption);
+        hasConfig = true;
+    }
+
+    // 如果没有通过参数或配置文件设置，使用头文件默认值
+    if (!hasConfig) {
+        mailConfig.smtpServer = SMTP_SERVER;
+        mailConfig.smtpPort = SMTP_SERVER_PORT;
+        mailConfig.senderName = SENDER_NAME;
+        mailConfig.senderEmail = SENDER_EMAIL;
+        mailConfig.senderPassword = SENDER_PASSWORD;
+        mailConfig.recipientName = RECIPIENT_NAME;
+        mailConfig.recipientEmail = RECIPIENT_EMAIL;
+        qDebug() << "使用头文件默认配置";
+    }
+
+    // 验证必需配置
+    if (mailConfig.smtpServer.isEmpty() || mailConfig.senderEmail.isEmpty() || 
+        mailConfig.senderPassword.isEmpty() || mailConfig.recipientEmail.isEmpty()) {
+        qDebug() << "错误: 缺少必需的邮件配置";
+        qDebug() << "请通过以下方式之一提供配置:";
+        qDebug() << "1. 配置文件: --config mail_config.ini";
+        qDebug() << "2. 命令行参数: --smtp-server, --sender-email, --sender-password, --recipient-email";
+        qDebug() << "3. 头文件: ../account_info.h (编译时配置)";
+        return -1;
+    }
+
+    if (parser.isSet(resetOption)) {
         QFile::remove(progressFile);
         qDebug() << "发送进度已重置";
     }
 
-    QString txtFile = argv[1];
-    qint64 interval = parseTimeInterval(argv[2]);
-    maxCount = QString(argv[3]).toInt();
-    linesPerEmail = QString(argv[4]).toInt();
+    QString txtFile = args[0];
+    qint64 interval = parseTimeInterval(args[1]);
+    maxCount = args[2].toInt();
+    linesPerEmail = args[3].toInt();
 
     if (interval <= 0) interval = 2000;
     if (maxCount <= 0) maxCount = 1;
@@ -236,3 +353,4 @@ int main(int argc, char *argv[]) {
 
     return app.exec();
 }
+
